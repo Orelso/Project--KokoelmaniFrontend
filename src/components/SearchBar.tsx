d353389fd1cd30e88a53bd3c727b80a72d9fecde
Orelso <orelso25@gmail.com>
2023-03-28 15:45:05 +0300

    modal

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,10 +4,8 @@ import {
   Box,
   Button,
   IconButton,
-  TableBody,
   TableCell,
   TableContainer,
-  TableHead,
   TableRow,
   TextField,
 } from "@mui/material";
@@ -21,12 +19,11 @@ import * as React from "react";
 import Typography from "@mui/material/Typography";
 import { red } from "@mui/material/colors";
 import { height } from "@mui/system";
-import type { DigimonCard, PokemonCard } from "../types";
-type SelectedCard = DigimonCard | PokemonCard;
+
 const FilterCard = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useAtom(searchResultsAtom);
-  const [selectedCard, setSelectedCard] = useState<SelectedCard | null>(null);
+  const [selectedCard, setSelectedCard] = useState(null);
   const [open, setOpen] = React.useState(false);
 
   const handleOnChange = ({
@@ -121,173 +118,41 @@ const FilterCard = () => {
 
       {selectedCard && (
         <Modal open={open} onClose={handleClose}>
-          <Paper
+          <Box
             sx={{
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
-              textAlign: "center",
-              margin: "auto",
-              marginTop: "64px",
-              justifyContent: "center",
-              height: "90%",
-              width: "80%",
-              border: "2px solid black",
             }}
           >
             <img
               src={selectedCard.image_uris?.normal || selectedCard.image_url}
               sx={{ mb: 2 }}
             />
+            <Typography variant="h6" sx={{ color: "red" }}>
+              {/* Digimon */}
+              {selectedCard.name}
+              {selectedCard.type}
+              {selectedCard.color}
+              {selectedCard.stage}
+              {selectedCard.digi_type}
+              {selectedCard.attribute}
+              {selectedCard.level}
+              {selectedCard.play_cost}
+              {selectedCard.evolution_cost}
+              {selectedCard.cardrarity}
+              {selectedCard.artist}
+              {selectedCard.dp}
+              {selectedCard.cardnumber}
+              {selectedCard.maineffect}
+              {selectedCard.set_name}
+              {selectedCard.card_sets}
+              {selectedCard.img_url}
+              {selectedCard.set_name}
 
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Property</TableCell>
-                  <TableCell>Value</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.name}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Type</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.type}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Color</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.color}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Stage</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.stage}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Digi Type</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.digi_type}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Attribute</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.attribute}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Level</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.level}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Play Cost</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.play_cost}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Evolution Cost</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.evolution_cost}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Card Rarity</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.cardrarity}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Artist</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.artist}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>DP</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.dp}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Card Number</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.cardnumber}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Main Effect</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.maineffect}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Set Name</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.set_name}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Card Sets</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.card_sets}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Card Img</TableCell>
-                  <TableCell>
-                    <Typography variant="h6" sx={{ color: "red" }}>
-                      {selectedCard.img_url}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </Paper>
-          \{" "}
+              {/* Pokemon */}
+            </Typography>
+          </Box>
         </Modal>
       )}
     </FilterCardStyles>
