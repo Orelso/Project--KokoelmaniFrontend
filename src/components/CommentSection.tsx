7320c066c19312c2a2d9e7665436939a31a1a762
Orelso <orelso25@gmail.com>
2023-04-17 12:37:45 +0300

    Comment Section basics complete

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 interface Comment {
   id: number;
   text: string;
+  username: string;
 }
 
 const CommentSection: React.FC = () => {
@@ -13,8 +14,9 @@ const CommentSection: React.FC = () => {
     const newComment: Comment = {
       id: comments.length + 1,
       text: newCommentText,
+      username: "exampleUsername",
     };
-    setComments([...comments, newComment]);
+    setComments([newComment, ...comments]); // new comments go to the top
     setNewCommentText("");
   };
 
@@ -23,7 +25,6 @@ const CommentSection: React.FC = () => {
       style={{
         border: "solid 5px hsla(0, 95%, 35%, 1)",
         borderRadius: "255px 15px 225px 15px/15px 225px 15px 255px",
-
         borderColor: "linear-gradient(to right, #fc5c7d, #6a82fb)",
         display: "flex",
         flexDirection: "column",
@@ -36,23 +37,40 @@ const CommentSection: React.FC = () => {
         marginRight: "1px",
       }}
     >
-      <h2>Comments</h2>
+      <h2
+        style={{
+          borderBottom: "1px solid black",
+          width: "100%",
+          textAlign: "center",
+          marginBottom: "2px",
+        }}
+      >
+        Comments
+      </h2>
       {comments.length === 0 ? (
         <p>No comments yet.</p>
       ) : (
         <ul style={{ listStyleType: "none", padding: 0 }}>
           {comments.map((comment) => (
-            <li key={comment.id}>{comment.text}</li>
+            <li key={comment.id}>
+              <strong>{comment.username}: </strong>
+              {comment.text}
+            </li>
           ))}
         </ul>
       )}
+      <div style={{ flex: 1 }} />
       <textarea
-        style={{ width: "100%", marginBottom: "10px" }}
+        style={{
+          width: "100%",
+          marginBottom: "10px",
+          border: "2px solid blue",
+        }}
         value={newCommentText}
         onChange={(e) => setNewCommentText(e.target.value)}
       />
       <button style={{ marginTop: "10px" }} onClick={handleAddComment}>
-        Add Comment
+        Post
       </button>
     </div>
   );
